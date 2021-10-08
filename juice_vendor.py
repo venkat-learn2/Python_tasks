4f0985a46b01fa766fc609ae975edbb47f8a8d65
AnusuyaRajendran <53506286+AnuRaj19@users.noreply.github.com>
2021-10-08 19:11:22 +0530

    Add files via upload

---
+++ juice_vendor.py
@@ -0,0 +1,72 @@
+stock_data=[
+{
+"id":1,
+"name":"mango",
+"qty":20,
+"price":10
+},
+{
+"id":2,
+"name":"orange",
+"qty":20,
+"price":10
+},
+{
+"id":3,
+"name":"grapes",
+"qty":20,
+"price":15
+},
+{
+"id":4,
+"name":"apple",
+"qty":20,
+"price":20
+}
+]
+currency_format=[1,2,5,10,20,50,100]
+print("1-mango,2-orange,3-grapes,4-apple")
+flavour=int(input("Enter your Flavour Number:"))
+qty=int(input("Enter the quantity:"))
+amt=int(input("Enter your Amount:"))
+def process_calculation(flavour,qty,amt,stock_data,currency_format):
+    if qty>20:
+        print("Only 20 Qty available.please enter 20 or below!")
+        return
+    else:
+        get_flavour_price=[x['price'] for x in stock_data if x['id']==int(flavour)]
+        total_purchased_price=qty*get_flavour_price[0]
+        remaining_amt=amt-total_purchased_price
+        # print("remaining_amt:",remaining_amt)
+        if remaining_amt>0:
+            currency_format.sort()
+            currency_format=currency_format[::-1]
+            get_change_amount=get_change(currency_format,remaining_amt)
+        else:
+            print("given Amount is insufficient!")
+
+def get_change(currency_format,remaining_amt):
+	return_arr=[]
+	for i in currency_format:
+		if remaining_amt in currency_format:
+			data={"amount":remaining_amt,"currency_qty":1}
+			return_arr.append(data)
+			break
+		if (remaining_amt>i):
+			if (remaining_amt%i)==0:
+				currency_qty=(remaining_amt//i)
+				return_amt=i
+				data={"amount":return_amt,"currency_qty":currency_qty}
+				return_arr.append(data)
+				break
+			else:
+				return_amt=i
+				currency_qty=(remaining_amt//i)
+				get_rem=(remaining_amt%i)
+				data={"amount":return_amt,"currency_qty":currency_qty}
+				# print("data:",data)
+				return_arr.append(data)
+				remaining_amt=get_rem
+	print("return:",return_arr)
+
+call_func=process_calculation(flavour,qty,amt,stock_data,currency_format)
---
+++ stud_details.py
@@ -0,0 +1,56 @@
+student_data=[]
+teacher_data=[]
+
+def create_student():
+	rollno=input("Enter your Roll no:")
+	name=input("Enter your name:")
+	age=input("Enter your age:")
+	dept=input("Enter your department")
+	student_creation=create_student_data(rollno,name,age,dept)
+	print({"status":1,"message":"created successfully"})
+	status=int(input("Enter 1 for student detail\nEnter 2 for Teacher detail\nEnter 3 to get student detail\nEnter 4 to get Teacher detail"))
+	check_status(status)
+def create_student_data(rollno,name,age,dept):
+	data={
+	"rollno":rollno,"name":name,"age":age,"dept":dept
+	}
+	student_data.append(data)
+
+def create_teacher():
+	staffid=input("Enter your staff id:")
+	name=input("Enter your name:")
+	age=input("Enter your age:")
+	dept=input("Enter your department")
+	staff_creation=create_staff_data(staffid,name,age,dept)
+	print({"status":1,"message":"created successfully"})
+	status=int(input("Enter 1 for student detail\nEnter 2 for Teacher detail\nEnter 3 to get student detail\nEnter 4 to get Teacher detail"))
+	check_status(status)
+def create_staff_data(staffid,name,age,dept):
+	data={
+	"staffid":staffid,"name":name,"age":age,"dept":dept
+	}
+	teacher_data.append(data)
+
+def get_student():
+	print(student_data)
+	status=int(input("Enter 1 for student detail\nEnter 2 for Teacher detail\nEnter 3 to get student detail\nEnter 4 to get Teacher detail"))
+	check_status(status)
+def get_teacher():
+	print(teacher_data)
+	status=int(input("Enter 1 for student detail\nEnter 2 for Teacher detail\nEnter 3 to get student detail\nEnter 4 to get Teacher detail"))
+	check_status(status)
+
+def check_status(status):
+	if status==1:
+		create=create_student()
+	elif status==2: 
+		create=create_teacher()
+	elif status==3:
+		get=get_student()
+	elif status==4:
+		get=get_teacher()
+	else:
+		print("Enter Valid status!")
+		
+status=int(input("Enter 1 for student detail\nEnter 2 for Teacher detail\nEnter 3 to get student detail\nEnter 4 to get Teacher detail"))
+check_status(status)
